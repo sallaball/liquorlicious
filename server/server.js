6226de48ce614c8fce10778d3bcf079e435f5d0e
sallaball <sallaball1206@gmail.com>
2022-10-27 20:05:36 -0500

    more packages, file structure & server.js

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+
+const { ApolloServer } = require('apollo-server-express');
+// const { typeDefs, resolvers } = require('./schemas');
+// const { authMiddleware } = require('./utils/auth'); 
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// const startServer = async () => {
+//     const server = new ApolloServer({
+//         typeDefs,
+//         resolvers,
+//         context: authMiddleware,
+//     });
+//     await server.start();
+//     server.applyMiddleware({ app });
+// };
+
+// startServer();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+db.once("open", () => {
+    app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
+});
