d18f8dca0f09e8f82d4aeee9ec45999d371c9d57
sallaball <sallaball1206@gmail.com>
2022-10-31 12:41:47 -0500

    attempting to fix connection issue

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth'); 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 
 const startServer = async (typeDefs, resolvers) => {
     const server = new ApolloServer({
