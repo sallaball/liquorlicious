8129793e2e824878e7b58bf92ea944a5f79d33d4
sallaball <sallaball1206@gmail.com>
2022-11-02 20:04:23 -0500

    added home.js and images folder

---
+++ client/src/components/Home.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import { Carousel } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// const imageSlider = () => {
+//     return(
+//         <div>
+//             <hr />
+//             <Carousel>
+//                 <Carousel.Item>
+//                     <img src={image1} height="300px" width="350px" alt="img1" /> 
+//                 </Carousel.Item>
+//             </Carousel>
+//         </div>
+//     )
+// }
+
+// export default imageSlider;
